f842b8d44b716d70094b2be1a64415a30025f629
Madhawa Monarawila <madawammonarawila@gmail.com>
2022-02-20 07:05:57 +0530

    Fix error at UserServiceImpl

--- src/main/java/com/madhawa/fullstack/service/UserServiceImpl.java
+++ src/main/java/com/madhawa/fullstack/service/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.madhawa.fullstack.service;
 import com.madhawa.fullstack.model.User;
 import com.madhawa.fullstack.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
